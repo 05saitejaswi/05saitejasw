f224cefe0e7e65b3a75bb0efd43c0b4f3f3d6d7a
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-10-09 17:02:56 +0530

    addition program
    addition program in c language.

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=2,b=3,sum;
+printf("enter the  two value");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum of entered number is :%d",sum);
+return 0;
+}
