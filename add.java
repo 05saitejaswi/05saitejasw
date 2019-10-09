1419a6e58e8018baf19a73e8698f121bc9d9f9ae
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-10-09 17:28:20 +0530

    addition in java
    addition  programe in java

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class AddNumber
+{
+public static void main(string args[]){
+int x,y,z;
+system.out.println("Enter two integers to calculate their sum");
+Scanner in=newScanner(System.in);
+x=in.nextlnt();
+y=in.nextlnt();
+z=x+y;
+System.out.println("sum of entered integers="+z);
+}
+}
